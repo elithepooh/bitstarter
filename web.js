a7a93c2cd777243fdc23564df260a7928d27d22f
Elitza Maneva <elitza.maneva@gmail.com>
2013-07-09 15:58:00 +0000

    web.js reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFile("index.html", function(err, data) {
+    data.toString('utf-8');  
+  });
 });
 
 var port = process.env.PORT || 5000;
